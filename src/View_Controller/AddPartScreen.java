8f7d52827faece6155b91f7fc3e0d68f2d29dd00
‘kcmodev’ <shchristensen3@gmail.com>
2020-07-14 11:23:25 -0500

    initial commit

--- src/View_Controller/AddPartScreen.java
+++ src/View_Controller/AddPartScreen.java
@@ -1,5 +1,14 @@
 package View_Controller;
 
+import javafx.event.ActionEvent;
+
 public class AddPartScreen {
 
+
+    public void setAddPartScreenSaveButton(ActionEvent event) { System.out.println("Add part save button clicked"); }
+
+    public void setAddPartScreenCancelButton(ActionEvent event) {
+
+        System.out.println("Add part screen cancel button clicked");
+    }
 }
---
+++ src/View_Controller/ModifyPartScreen.java
@@ -0,0 +1,14 @@
+package View_Controller;
+
+import java.awt.event.ActionEvent;
+
+public class ModifyPartScreen {
+
+    public void setModPartScreenSaveButton(ActionEvent event){
+        System.out.println("Mod screen save button clicked");
+    }
+
+    public void setModPartScreenCancelButton(ActionEvent event){
+        System.out.println("Mod screen cancel button clicked");
+    }
+}
